7215f056c1ab068c9be58726e8d275907b5183b9
Tad Taylor <bittiez@gmail.com>
2017-03-17 19:05:48 -0600

    Added SQL class

---
+++ src/US/bittiez/TotalTracker/SQLTABLE.java
@@ -0,0 +1,12 @@
+package US.bittiez.TotalTracker;
+
+/**
+ * Created by gamer on 3/17/2017.
+ */
+public class SQLTABLE {
+    public final static String PLAYER = "player";
+    public final static String PVP_KILLS = "pvp_kills";
+    public final static String MOB_KILLS = "mob_kills";
+    public final static String BLOCKS_BROKEN = "blocks_broken";
+    public final static String BLOCKS_PLACED = "blocks_places";
+}
