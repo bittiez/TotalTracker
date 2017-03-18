c94e09b011fbdfe6ae80e88546f87c932fc19213
Tad Taylor <bittiez@gmail.com>
2017-03-17 18:15:01 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+#KillTracker
+
+Spigot 1.11 plugin to keep track of various kill stats
+
+
+#Description
+Note: This requires Java 1.8+
+
+This will keep track of players pvp kills and deaths, and also keep track of individual monster kills.
+
+#Usage
+
+Kill or be killed.
+
+#Permissions
+
+None, may add them upon request.
+
+
+#Installation
+
+- Place the jar file in your plugins folder
+- Restart your server
+
+
+#Configuration
+
+Currently no configuration options, may add message templates later.
---
+++ src/US/bittiez/TotalTracker/main.java
@@ -0,0 +1,50 @@
+package US.bittiez.TotalTracker;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+//
+// Plan
+// Main Thread
+// Every 5? minutes create a processor thread, send the array list to it and clear the original
+
+public class main extends JavaPlugin implements Listener{
+    private static Logger log;
+
+    @Override
+    public void onEnable() {
+        log = getLogger();
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent e){
+        if(e.getEntity().getLastDamageCause() instanceof EntityDamageByEntityEvent) {
+            EntityDamageByEntityEvent nEvent = (EntityDamageByEntityEvent) e.getEntity().getLastDamageCause();
+            Entity ekiller = nEvent.getDamager();
+            if (ekiller instanceof Player) {
+                //Player killed entity
+            }
+        }
+
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+        Player victim = e.getEntity();
+        Player killer = victim.getKiller();
+        if(killer != null){
+
+        }
+    }
+}
