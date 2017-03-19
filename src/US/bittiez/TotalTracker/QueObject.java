7785c6f438ee3c82475ba498af28d59e3b3e32b9
Tad Taylor <bittiez@gmail.com>
2017-03-19 14:00:37 -0600

    QurProccessor should now combine Que Items to use less MySQL queries

--- src/US/bittiez/TotalTracker/QueObject.java
+++ src/US/bittiez/TotalTracker/QueObject.java
@@ -4,6 +4,7 @@ package US.bittiez.TotalTracker;
 public class QueObject {
     public String QueType;
     public String Player;
+    public int Quantity = 1;
 
     public QueObject(String PlayerUUID, String queType){
         Player = PlayerUUID;
--- src/US/bittiez/TotalTracker/QueProcessor.java
+++ src/US/bittiez/TotalTracker/QueProcessor.java
@@ -1,5 +1,6 @@
 package US.bittiez.TotalTracker;
 
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
@@ -9,8 +10,11 @@ public class QueProcessor extends BukkitRunnable {
     public ArrayList<QueObject> QueObjects;
     public Logger log;
 
+    private ArrayList<QueObject> ConsolidatedQueObjects;
+
     public QueProcessor(ArrayList<QueObject> QueObjects, Logger log){
         this.QueObjects = QueObjects;
+        ConsolidatedQueObjects = new ArrayList<>();
         this.log = log;
     }
 
@@ -20,6 +24,29 @@ public class QueProcessor extends BukkitRunnable {
         if(QueObjects != null && QueObjects.size() > 0){
             if(main.debug)
                 log.info("Trying to process a que of " + QueObjects.size() + " QueObjects.");
+
+            for(QueObject o : QueObjects){
+                Boolean addNew = true;
+
+                //Combine same QueTypes
+                for(QueObject co : ConsolidatedQueObjects){
+                    if (co.Player == o.Player) {
+                        if (co.QueType == o.QueType) {
+                            co.Quantity += o.Quantity;
+                            addNew = false;
+                        }
+                    }
+                }
+
+                if(addNew) {
+                    ConsolidatedQueObjects.add(o);
+                }
+            }
+            //Done consolidating, time to do the MySQL
+            for(QueObject co : ConsolidatedQueObjects) {
+                if(main.debug)
+                    log.info("Saving [" + co.Quantity + "] x [" + co.QueType + "] for [[UUID]" + co.Player + "]");
+            }
         }
     }
 }
