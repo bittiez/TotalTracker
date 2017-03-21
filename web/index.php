9ecde752dc255538fcd79334b24b41c48bc4645e
Tad Taylor <bittiez@gmail.com>
2017-03-20 19:01:07 -0600

    More work on the web portion

--- web/index.php
+++ web/index.php
@@ -14,7 +14,8 @@ require_once('config.php');
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <link href="css/sb-admin.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -24,28 +25,49 @@ require_once('config.php');
     <![endif]-->
   </head>
   <body>
-    <h1>Hello, world! Stats here!</h1>
+    <div class="container-fluid">
+      <div id="page-wrapper">
+
+      <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                TotalTracker Stats
+            </h1>
+        </div>
+    </div>
 
 
     <div class="row">
-      <div class="col-sm-4">
-        <table class="table table-striped">
-          <thead>
-            <td colspan="2">Blocks Broken</td>
-          </thead>
-          <tbody>
-            <td>PlayerName</td>
-            <td>1234</td>
-          </tbody>
-        </table>
+      <div class="col-sm-3">
+        <h2>Blocks Broken</h2>
+          <div class="table-responsive">
+              <table class="table table-bordered table-hover">
+                  <thead>
+                      <tr>
+                          <th>Player</th>
+                          <th>Blocks Broken</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr>
+                          <td>/index.html</td>
+                          <td>1265</td>
+                      </tr>
+                      <tr>
+                          <td>/about.html</td>
+                          <td>261</td>
+                      </tr>
+                  </tbody>
+              </table>
+          </div>
       </div>
-      <div class="col-sm-4"></div>
-      <div class="col-sm-4"></div>
+      <div class="col-sm-3"></div>
+      <div class="col-sm-3"></div>
     </div>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+</div>
+</div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
