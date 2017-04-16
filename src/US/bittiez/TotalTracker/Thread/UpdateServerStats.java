09b834e15a4d7241c67a272bbf90cb3724ae90f9
Tad Taylor <bittiez@gmail.com>
2017-04-15 18:02:48 -0600

    Command tt cache will populate server stats with current data in db

--- src/US/bittiez/TotalTracker/Thread/UpdateServerStats.java
+++ src/US/bittiez/TotalTracker/Thread/UpdateServerStats.java
@@ -2,6 +2,7 @@ package US.bittiez.TotalTracker.Thread;
 
 import US.bittiez.TotalTracker.Models.TotalStats;
 import US.bittiez.TotalTracker.SQLTABLE;
+import US.bittiez.TotalTracker.Sql.Stats;
 import US.bittiez.TotalTracker.main;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.sql2o.Connection;
@@ -29,12 +30,21 @@ public class UpdateServerStats implements Runnable{
             }
         if(totalStats != null){
             if(main.debug)
-                log.info("Total Stats ID: " + totalStats.id); //Working
-//            UPDATE `teTotalTracker_server` c
-//            INNER JOIN (
-//                    SELECT SUM(pvp_kills) as total FROM `teTotalTracker`
-//            ) x
-//            SET c.pvp_kills = x.total
+                log.info("Total Stats ID: " + totalStats.id);
+            String sql = "UPDATE "
+                    + SQLTABLE.genFullTableSQL(true) + " c "
+                    + "INNER JOIN ( SELECT SUM(%s) as total FROM " + SQLTABLE.genFullTableSQL() + " ) x "
+                    + "SET c.%<s = x.total;";
+            try (Connection con = SQL.open()) {
+                for (Stats stat : Stats.values()) { //Loop through the stats
+                    if (stat.equals(Stats.PLAYER))
+                        continue;
+                    String compiledSql = String.format(sql, stat.toString());
+                    if (main.debug)
+                        log.info("RUN SQL: " + compiledSql);
+                    con.createQuery(compiledSql).executeUpdate();
+                }
+            }
         }
     }
 }
