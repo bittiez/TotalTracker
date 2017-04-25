b3bb5a69e231eaff256b593b40c6498bfbd1bb99
Tad Taylor <bittiez@gmail.com>
2017-04-25 14:16:03 -0600

    Changed from int to double to support vault balances

--- src/US/bittiez/TotalTracker/QueObject.java
+++ src/US/bittiez/TotalTracker/QueObject.java
@@ -6,7 +6,7 @@ import org.bukkit.entity.Player;
 public class QueObject {
     public String QueType;
     public String PlayerUUID;
-    public int Quantity = 1;
+    public double Quantity = 1;
     public String PlayerName;
     public boolean sentToDataBase = false;
 
@@ -23,12 +23,23 @@ public class QueObject {
         this.Quantity = Quantity;
     }
 
+    public QueObject(String PlayerUUID, String queType, String PlayerName, double Quantity) {
+        this.PlayerUUID = PlayerUUID;
+        this.QueType = queType;
+        this.PlayerName = PlayerName;
+        this.Quantity = Quantity;
+    }
+
 
     public QueObject(Player player, String queType) {
         this(player.getUniqueId().toString(), queType, player.getName());
     }
 
     public QueObject(Player player, String queType, int Quantity) {
+        this(player.getUniqueId().toString(), queType, player.getName(), Quantity);
+    }
+
+    public QueObject(Player player, String queType, double Quantity) {
         this(player.getUniqueId().toString(), queType, player.getName(), Quantity);
     }
 }
